400ab3c95533e7b705c28b71e6a787abc99c0da3
JuLoSpace <julospace@gmail.com>
2024-07-15 22:22:58 +0500

    add BrainLink libs

--- android/app/src/main/java/com/bdc/neurotrainer/MainActivity.java
+++ android/app/src/main/java/com/bdc/neurotrainer/MainActivity.java
@@ -1,6 +1,19 @@
 package com.bdc.neurotrainer;
 
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+
+import com.clj.fastble.BleManager;
+import com.clj.fastble.callback.BleNotifyCallback;
+import com.clj.fastble.callback.BleScanAndConnectCallback;
+import com.clj.fastble.data.BleDevice;
+import com.clj.fastble.exception.BleException;
+import com.clj.fastble.scan.BleScanRuleConfig;
+import com.macrotellect.gs5001.EEGParse;
+import com.macrotellect.gs5001.callBack.OnDataCallBack;
+import com.macrotellect.gs5001.callBack.OnSignCallBack;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "brainlink/data";
 }
