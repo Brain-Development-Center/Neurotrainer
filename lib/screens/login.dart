e9c511ff53b97664ff104cc82ad7ed91f13baf77
JuLoSpace <julospace@gmail.com>
2024-07-17 00:27:05 +0500

    add firebase to all platforms

---
+++ lib/screens/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+
+class LoginScreen extends StatefulWidget {
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
