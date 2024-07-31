c1b8dcd573d68bd89a7fca4e3abe803c83d74047
JuLoSpace <julospace@gmail.com>
2024-07-31 20:38:30 +0500

    update select mode screen

--- lib/screens/select_mode.dart
+++ lib/screens/select_mode.dart
@@ -19,55 +19,95 @@ class _SelectModeScreenState extends State<SelectModeScreen> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            alignment: Alignment.topRight,
-            margin: EdgeInsets.only(top: height * 0.1),
-            child: SvgPicture.asset('assets/select_mode.svg', fit: BoxFit.cover, height: height * 0.4,),
-          ),
-          Container(
-            width: width * 0.5,
-            margin: EdgeInsets.only(top: height * 0.05),
-            child: ElevatedButton(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Свободный режим', style: GoogleFonts.jost(color: Colors.white, fontSize: 20)),
+          Stack(
+            children: [
+              Container(
+                height: height,
+                width: width,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: SvgPicture.asset('assets/select_mode_1.svg', width: width * 0.5,),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: height * 0.05),
+                      alignment: Alignment.centerRight,
+                      child: SvgPicture.asset('assets/select_mode_2.svg', width: width * 0.2,)
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: height * 0.05),
+                      alignment: Alignment.centerRight,
+                      child: SvgPicture.asset('assets/select_mode_2.svg', width: width * 0.2,)
+                    ),
+                    Expanded(
+                      child: Container(
+                        alignment: Alignment.bottomRight,
+                        child: SvgPicture.asset('assets/select_mode_3.svg', width: width * 0.8,)
+                      ),
+                    )
+                  ],
+                ),
               ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF86B0CB),
-                padding: EdgeInsets.only(top: 16, bottom: 16, left: 16)
+              Container(
+                height: height,
+                width: width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: SvgPicture.asset('assets/select_mode_4.svg', width: width * 0.2,),
+                    ),
+                    Container(
+                      width: width * 0.5,
+                      margin: EdgeInsets.only(top: height * 0.05),
+                      child: ElevatedButton(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Свободный режим', style: GoogleFonts.jost(color: Colors.white, fontSize: 20)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF367CA9),
+                            padding: EdgeInsets.only(top: 16, bottom: 16, left: 16)
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      width: width * 0.5,
+                      margin: EdgeInsets.only(top: height * 0.03),
+                      child: ElevatedButton(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Создать профиль', style: GoogleFonts.jost(color: Colors.white, fontSize: 20)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF367CA9),
+                            padding: EdgeInsets.only(top: 16, bottom: 16, left: 16)
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    Container(
+                      width: width * 0.5,
+                      margin: EdgeInsets.only(top: height * 0.03),
+                      child: ElevatedButton(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text('Выбор профиля', style: GoogleFonts.jost(color: Colors.white, fontSize: 20)),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF367CA9),
+                            padding: EdgeInsets.only(top: 16, bottom: 16, left: 16)
+                        ),
+                        onPressed: () {},
+                      ),
+                    )
+                  ],
+                )
               ),
-              onPressed: () {},
-            ),
-          ),
-          Container(
-            width: width * 0.5,
-            margin: EdgeInsets.only(top: height * 0.03),
-            child: ElevatedButton(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Создать профиль', style: GoogleFonts.jost(color: Colors.white, fontSize: 20)),
-              ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF86B0CB),
-                  padding: EdgeInsets.only(top: 16, bottom: 16, left: 16)
-              ),
-              onPressed: () {},
-            ),
-          ),
-          Container(
-            width: width * 0.5,
-            margin: EdgeInsets.only(top: height * 0.03),
-            child: ElevatedButton(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Выбор профиля', style: GoogleFonts.jost(color: Colors.white, fontSize: 20)),
-              ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF86B0CB),
-                  padding: EdgeInsets.only(top: 16, bottom: 16, left: 16)
-              ),
-              onPressed: () {},
-            ),
+            ],
           )
         ],
       ),
